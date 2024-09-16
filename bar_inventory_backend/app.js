e98377aae06b47a3e74f608c9ba9d4209ec7171d
Robert Roger <robert.roger@DIGITALCOLLEGE.LOCAL>
2024-09-16 17:57:58 -0300

    backend

--- bar_inventory_backend/app.js
+++ bar_inventory_backend/app.js
@@ -1,5 +1,3 @@
-// app.js
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const apiRoutes = require('./routes/api');
@@ -10,8 +8,13 @@ const app = express();
 app.use(bodyParser.json());
 app.use('/api', apiRoutes);
 
+// Rota raiz
+app.get('/', (req, res) => {
+  res.send('Bem-vindo ao Drink Inventory!');
+});
+
 // Inicializa o servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`Servidor rodando na porta http://localhost:${PORT}`);
 });
